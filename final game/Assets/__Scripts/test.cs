f48f0ce4c56398bc550b2f706408542e42df3dc8
tchen127 <107194861+tchen127@users.noreply.github.com>
2024-03-13 13:26:00 -0400

    Testing Git

--- final game/Assets/__Scripts/test.cs
+++ final game/Assets/__Scripts/test.cs
@@ -7,7 +7,7 @@ public class test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Console.WriteLine("Testing Git");
+        Console.WriteLine("Testing Git, try to change something");
     }
 
     // Update is called once per frame
