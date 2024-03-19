ab875378372462bf09a64ea261a28b30d0ee9354
Reed Shoolbred <reedshoolbred@gmail.com>
2024-03-19 14:26:24 -0400

    Created Example Scene
    basic layout of an example scene, now need to implement follow cam and player movement

--- final game/Assets/__Scripts/FollowCam.cs
+++ final game/Assets/__Scripts/FollowCam.cs
@@ -2,20 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-/// Miles Edit
-/// Grace Edit 
-
-public class test : MonoBehaviour
+public class FollowCam : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Testing Github");
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 }
