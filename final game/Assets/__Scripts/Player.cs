f0789a9ce573a46d1da0ca7caf98304f76ff346f
Reed Shoolbred <reedshoolbred@gmail.com>
2024-04-15 19:38:55 -0400

    Deleted unused parts of BoundsCheck

--- final game/Assets/__Scripts/Player.cs
+++ final game/Assets/__Scripts/Player.cs
@@ -7,13 +7,15 @@ public class Player : MonoBehaviour
 {
     private bool isGrounded = false;
     private Rigidbody2D rb;
-    private float playerHeight;
+    
     private RaycastHit2D hit2D;
     private Vector2 jumpVec;
 
 
     [SerializeField] private float speed = 20;
     [SerializeField] private float jumpSpeed = 5;
+    [SerializeField] private float playerHeight;
+    [SerializeField] private float playerWidth;
     [SerializeField] private LayerMask layerMask;
 
 
@@ -22,9 +24,12 @@ public class Player : MonoBehaviour
         //get rigidbody component
         rb = GetComponent<Rigidbody2D>();
 
+        //set player height and width based on player gameobject
+        playerHeight = transform.localScale.y;
+        playerWidth = transform.localScale.x;
+
         //set direction and magnitude of jump
         jumpVec = rb.velocity + new Vector2(0, jumpSpeed);
-
     }
 
     // Update is called once per frame
@@ -73,5 +78,31 @@ public class Player : MonoBehaviour
             isGrounded = false;
             Debug.Log("isGrounded: " + isGrounded);
         }
+    }
+
+    /// <summary>
+    /// Return true if player is at the side of the screen
+    /// Can alter width of character to get closer/farther from the border
+    /// </summary>
+    /// <returns>True if player is at side border of screen, otherwise false</returns>
+    private bool atSideOfScreen()
+    {
+        //get coordinates of left and right border of screen
+        Vector3 rightEdge = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, 1));
+        Vector3 leftEdge = new Vector3 (rightEdge.x * -1, 0, 1);
+
+        //subtract player width from boundaries. this determines how close player can get to the edge
+        float maxXPos = rightEdge.x - (playerWidth / 2);
+        float minXPos = leftEdge.x + (playerWidth / 2);
+
+        //player position
+        Vector3 pos = this.transform.position;
+
+        //return true if player is at border, otherwise false
+        if (pos.x < minXPos || pos.x > maxXPos){
+            return true;
+        }
+
+        return false;
     }
 }
