69a4642bf478dea9770e75762a5d6cca0678b2f3
Reed Shoolbred <reedshoolbred@gmail.com>
2024-04-23 01:16:58 -0400

    Rocket bug fix
    Fixed Bug that allowed player to win by falling straight down. Commented Player script

--- final game/Assets/__Scripts/Rocket.cs
+++ final game/Assets/__Scripts/Rocket.cs
@@ -16,19 +16,49 @@ public class Rocket : MonoBehaviour
     public GameObject followerCount;
     // tweakable number of followers the player must have in order to win the level
     public int requiredFollowersToWin;
+    //Collider2D attached to this GameObject
+    private Collider2D coll;
+    //minimum y position needed to activate the collider
+    private float minYVal;
+    //height of the rocket's collider, used to determine when to enable collider
+    private float rocketHeight;
+
+    void Start()
+    {
+        //disable the collider for this gameobject until it enters the screen
+        coll = GetComponent<BoxCollider2D>();
+        rocketHeight = coll.bounds.size.y;
+        coll.enabled = false;
+
+        //get the minimum y position needed to activate the collider
+        //this is the y value at the bottom of the screen
+        minYVal = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 0)).y;
+    }
+
+    /// <summary>
+    /// Enable the rocket's collider only if it has entered the screen
+    /// </summary>
+    void Update()
+    {
+        if (transform.position.y > (minYVal - (rocketHeight/2))){
+            coll.enabled = true;
+        }
+    }
 
     /// <summary>
     /// when colliding with the player, end the level. if the player met or exceeded required follower count, they win. if they don't, they lose. 
     /// </summary>
     /// <param name="collision"></param>
-    private void OnTriggerEnter2D(Collider2D collision) 
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
             if (followerCount.GetComponent<FollowerCount>().followerCount >= requiredFollowersToWin)
             {
                 SceneManager.LoadScene("You_Win");
-            } else{
+            }
+            else
+            {
                 SceneManager.LoadScene("GameOverNotEnoughFollowers");
             }
         }
